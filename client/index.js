a4a1343393c0b3287c67f7412e17a18c72cd9268
williamg1750 <william.guan12@gmail.com>
2021-03-20 16:47:25 -0400

    whiteboard

--- client/index.js
+++ client/index.js
@@ -5,6 +5,8 @@ import {Router} from 'react-router-dom'
 import history from './history'
 import store from './store'
 import App from './app'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import reportWebVitals from './reportWebVitals'
 
 // establishes socket connection
 import './socket'
@@ -17,3 +19,13 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('app')
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
--- server/socket/index.js
+++ server/socket/index.js
@@ -2,6 +2,8 @@ module.exports = io => {
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
+    socket.on('drawing', data => socket.broadcast.emit('drawing', data))
+
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
     })
